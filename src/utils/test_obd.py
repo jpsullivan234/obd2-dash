0bf1217b1023147809d233fac0dcfc52122d63d5
John Sullivan <jpsullivan234@icloud.com>
2025-06-15 10:55:41 -0500

    added code to print out all supported commands in test_obd.py

--- src/utils/test_obd.py
+++ src/utils/test_obd.py
@@ -3,10 +3,9 @@ import obd
 # create connection
 connection = obd.OBD('/dev/ttyUSB0')
 
-# select a command
-cmd = obd.commands.RPM
-
-# send the command
-response = connection.query(cmd)
-
-print(f"RPM: {response.value}")
+if connection.is_connected():  # Ensure a connection is established.
+    supported_cmds = connection.supported_commands
+    for command in supported_cmds:
+        print(command.name)  # Print the name of each supported command
+else:
+    print("OBD-II adapter not connected or car ignition is off.")
