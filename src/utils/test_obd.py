f221885216adeba0c95f58d3a638bc7e68f30f81
John Sullivan <jpsullivan234@icloud.com>
2025-06-15 20:07:00 -0500

    testing querying of different commands

--- src/utils/test_obd.py
+++ src/utils/test_obd.py
@@ -3,9 +3,18 @@ import obd
 # create connection
 connection = obd.OBD('/dev/ttyUSB0')
 
-if connection.is_connected():  # Ensure a connection is established.
-    supported_cmds = connection.supported_commands
-    for command in supported_cmds:
-        print(command.name)  # Print the name of each supported command
-else:
-    print("OBD-II adapter not connected or car ignition is off.")
+# select a command
+cmd = obd.commands.DTC_MAF
+
+# send the command
+response = connection.query(cmd)
+
+print(response.value)
+
+
+# if connection.is_connected():  # Ensure a connection is established.
+#     supported_cmds = connection.supported_commands
+#     for command in supported_cmds:
+#         print(command.name)  # Print the name of each supported command
+# else:
+#     print("OBD-II adapter not connected or car ignition is off.")
